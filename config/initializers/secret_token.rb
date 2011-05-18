415d91b39a3378318c3c8860710cfcf9b7c56df4
Jessica Eldredge <jessica.eldredge@livingsocial.com>
2011-05-18 16:22:14 -0400

    Adding Fancy Pants

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyPants::Application.config.secret_token = '19b2da2eeb81982dfeb840e1534833d0c047eb982b080709f139a373ae735ba877df66c5f9c713b843227b1f72cc4a5b9e4865c1b2a10e49406b76a9272d30de'
